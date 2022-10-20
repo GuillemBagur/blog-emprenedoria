5c592f3e93d7ef467027f598762f4f13e5b00cf9
Guillem Bagur <guillembagurmoll@gmail.com>
2022-10-20 22:03:44 +0200

    Final version

---
+++ scripts.js
@@ -0,0 +1,30 @@
+const renderPosts = posts => {
+    const dump = document.getElementById("blog-dump");
+    for(let post of posts) {
+        let postEl = `<div class="post">
+            <h2 class="post__title">${post.title}</h2>
+            <h3 class="post__author">${post.author}</h3>
+            <h4 class="post__date">${post.date}</h4>
+            <p class="post__body">
+                ${post.body}
+            </p>
+        </div>`;
+
+        dump.innerHTML += postEl;
+    }
+}
+
+
+const loadPosts = async () => {
+  const data = await fetch(
+    `https://busy-teal-panther-cape.cyclic.app/get-posts`
+  );
+  const posts = await data.json();
+  
+  return posts;
+};
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const posts = await loadPosts();
+  renderPosts(posts);
+});
