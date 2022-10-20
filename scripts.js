5111103cc37ff91996fa911668a3cdd83f7e2519
Guillem Bagur <guillembagurmoll@gmail.com>
2022-10-20 22:08:32 +0200

    Changing the API for titles

--- scripts.js
+++ scripts.js
@@ -1,8 +1,24 @@
+const parseWeekDay = (day) => {
+  day = day.toLowerCase();
+  const days = {
+    monday: "Dilluns",
+    tuesday: "Dimarts",
+    wednesday: "Dimecres",
+    thursday: "Dijous",
+    friday: "Divendres",
+    saturday: "Dissabte",
+    sunday: "Diumenge"
+  }
+
+  return days[day] ?? day;
+}
+
+
 const renderPosts = posts => {
     const dump = document.getElementById("blog-dump");
     for(let post of posts) {
         let postEl = `<div class="post">
-            <h2 class="post__title">${post.title}</h2>
+            <h2 class="post__title">${parseWeekDay(post.title)}</h2>
             <h3 class="post__author">${post.author}</h3>
             <h4 class="post__date">${post.date}</h4>
             <p class="post__body">
