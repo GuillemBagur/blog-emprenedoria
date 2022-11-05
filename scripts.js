770fd403d91fe1ed51e2290520217d41f31ea718
Guillem Bagur <guillembagurmoll@gmail.com>
2022-11-05 15:48:05 +0100

    bugs fixed!

--- scripts.js
+++ scripts.js
@@ -1,19 +1,20 @@
 const renderPosts = (posts, editable = false) => {
   console.log(editable);
   const dump = document.getElementById("blog-dump");
-  
+
   for (let post of posts) {
     const postID = posts.indexOf(post);
-    const editAction = editable ? 
-    `<button onclick="editArticle(${postID})">Editar</button>`
-    : "";
+    const contentEditable = editable ? `contenteditable=true` : "";
+    const editAction = editable
+      ? `<button onclick="editArticle(${postID})">Editar</button>`
+      : "";
 
     let postEl = `<div class="post" data-id="${postID}">
-            <h2 class="post__title">${post.title}</h2>
+            <h2 ${contentEditable} class="post__title">${post.title}</h2>
             <h3 class="post__author">Autor/a:
-              <span class="post__author-name">${post.author}</span></h3>
+              <span ${contentEditable} class="post__author-name">${post.author}</span></h3>
             <h4 class="post__date">${post.date}</h4>
-            <p contenteditable="true" class="post__body">
+            <p ${contentEditable} class="post__body">
                 ${post.body}
             </p>
             ${editAction}
